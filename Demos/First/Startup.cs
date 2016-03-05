05dad1393422f7ef888f9a0dfd8f6d232f191b27
Pedro Felix <pmhsfelix@gmail.com>
2016-03-05 15:34:42 +0000

    adds demos for 2016/03/02

---
+++ Demos/First/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace First
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                "route name",
+                "api/{controller}/{id:int}",
+                new { id = RouteParameter.Optional });
+            config.Routes.MapHttpRoute(
+                "route name 2",
+                "api/{controller}/{id2}",
+                new { id2 = RouteParameter.Optional });
+            app.UseWebApi(config);
+        }
+    }
+}
